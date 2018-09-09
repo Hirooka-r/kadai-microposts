86e420aa6f5772b0e4d3c923d9aebdd9fa648fc0
hirooka <ryuzoyo@kkmail.be>
2018-09-09 14:28:36 +0000

    for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,15 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :like_posts, through: :favorites, source: :micropost
 
   def follow(other_user)
     unless self == other_user
@@ -27,7 +30,21 @@ class User < ApplicationRecord
     self.followings.include?(other_user)
   end
   
+  def likes(other_micropost)
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+  
+  def unlike(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def liking?(other_micropost)
+    self.like_posts.include?(other_micropost)
+  end
+  
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
 end
